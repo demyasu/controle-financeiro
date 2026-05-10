489eb8ffaabb422a6e01a37a6a3f07d5da85ffee
demyasu <demyasu@gmail.com>
2026-05-10 19:22:46 -0300

    Fix: add net-smtp gem, switch puma to single mode

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
-workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
 threads_count = Integer(ENV.fetch('MAX_THREADS', 5))
 threads threads_count, threads_count
 
 port        ENV.fetch('PORT', 8080)
 environment ENV.fetch('RACK_ENV', 'production')
+
+single_mode
