3d8f9dc80dfcd64dcc60ba1bd7741c4a72c6083c
demyasu <demyasu@gmail.com>
2026-05-10 19:32:10 -0300

    Fix: use Dir.pwd for migrations path, add debug logging

--- config/database.rb
+++ config/database.rb
@@ -13,4 +13,7 @@ end
 DB = Sequel.connect(database_url)
 
 Sequel.extension :migration
-Sequel::Migrator.run(DB, File.join(__dir__, '..', 'db', 'migrations'))
+migration_dir = ENV['MIGRATIONS_PATH'] || File.join(Dir.pwd, 'db', 'migrations')
+puts "[DB] Running migrations from: #{migration_dir}"
+puts "[DB] Files: #{Dir.glob(File.join(migration_dir, '*.rb')).inspect}"
+Sequel::Migrator.run(DB, migration_dir)
